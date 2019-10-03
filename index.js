b79e512935e3aba160892f5a9f59172ce2601e22
Cameron Steele <Cam.steeleis@gmail.com>
2019-10-02 20:30:32 -0500

    Inital Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+const express = require('express');
+
+let app = express();
+
+let value = 0;
+
+app.get("/add", (req, res) => {
+    res.send(++value);
+})
+
+app.get("/subtract", (req, res) => {
+    res.send(--value);
+})
+
+app.listen(3000)
